ea5d65daeb4f76f64ac7c5430db4d8916b290d25
Aj16r1 <Aj16r2@gmail.com>
2024-12-18 18:05:43 +0000

    Still adding files

--- main.py
+++ main.py
@@ -10,3 +10,23 @@ def addStock(name,ticker,sector,numOfShares):
     price = round(float(stock_info.get_live_price(ticker)),2)
     newStock = Stock(name,ticker,sector,price,numOfShares)
     portfolio.append(newStock)
+
+def updatePrice():
+    for stock in portfolio:
+        price = stock_info.get_live_price(stock.ticker)
+
+def viewPortfolio():
+    print("{0:20s}{1:10s}{2:35s}{3:14s}{4:6s}{5:10s}".format("Name of Stock","Ticker","Industry","Price","QTY","Gain/Loss"))
+    count =1
+    for stock in portfolio:
+        print(f"{count}. {stock.name:{17}}{stock.ticker:{10}}{stock.sector:{25}}{stock.currentPrice:{8}}{stock.numOfShares:{6}}")
+        count+=1
+
+for i in range(5):
+    name =input("Enter the Name of the Stock: ")
+    ticker = input("Enter the stock ticker name: ")
+    tick = yf.Ticker(ticker).info
+    sector=tick['industry']
+    numOfShares= int(input("Enter number of stock shares to buy: "))
+    addStock(name,ticker,sector,numOfShares)
+viewPortfolio()
